fc98ec17d9fb9b785fb474bec2f3cedf748103e8
Yoe Cardenas <yoe.cardenas@cun.edu.co>
2020-07-30 22:37:02 -0500

    Se crea proyecto angular, se agregan componentes y se descarga la libreria de animate.css y se referencia en el archivo de angular.json para utilizar

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './components/pages/heroe/heroe.component';
+import { HeroesComponent } from './components/pages/heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
